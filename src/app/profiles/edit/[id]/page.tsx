c1c1a3760d720b45804371e0e7ff2c2638aae379
arman-soravin <arman.aram12@gmail.com>
2026-09-07 23:46:14 +0330

    Create page.tsx

---
+++ src/app/profiles/edit/[id]/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Edit } from "@refinedev/antd";
+import { Form, Input, Select } from "antd";
+import { Authenticated } from "@refinedev/core";
+
+export default function ProfileEdit() {
+  return (
+    <Authenticated>
+      <Edit title="ویرایش کاربر">
+        <Form layout="vertical">
+          <Form.Item label="نام کاربری" name="username">
+            <Input />
+          </Form.Item>
+          <Form.Item label="تلفن" name="phone">
+            <Input />
+          </Form.Item>
+          <Form.Item label="آدرس" name="address">
+            <Input.TextArea rows={4} />
+          </Form.Item>
+          <Form.Item label="نقش" name="role">
+            <Select>
+              <Select.Option value="user">user</Select.Option>
+              <Select.Option value="admin">admin</Select.Option>
+            </Select>
+          </Form.Item>
+        </Form>
+      </Edit>
+    </Authenticated>
+  );
+}
